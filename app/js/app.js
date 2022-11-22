42a156cb0ec143f83a54cc04a986676062c3909e
islom-karimov <islom.kdev@gmail.com>
2022-11-22 13:51:49 +0500

    first commit

---
+++ app/js/app.js
@@ -0,0 +1,11 @@
+// import Bootstrap from 'bootstrap';
+// import Fancybox from 'fancybox';
+// Swiper.js
+import Swiper from 'swiper';
+
+// Gsap.js
+import Gsap from 'gsap'
+
+document.addEventListener('DOMContentLoaded', () => {
+    gsap.to({});
+}) 
